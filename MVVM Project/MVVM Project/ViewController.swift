4d162af06cb2c1b6159185e16c2fcea4176a6626
Komang Sidhi Artha <sidhi.artha@gmail.com>
2019-01-27 18:44:48 +0700

    init project

---
+++ MVVM Project/MVVM Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Project
+//
+//  Created by Sidhi Artha on 27/01/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
